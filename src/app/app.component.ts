6cbfb52a0c29fe698e47c578fb4343fa18ca9109
gpavankmr7 <gpavankmr7@gmail.com>
2019-07-30 21:44:10 +0530

    ineterpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyApp';
+  serverName='MyServer';
+  serverId=10;
+  serverStatus=true;
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+
 }
